34c1586ac4314148a8772d1b9d4ca0b189f98f37
Dat004 <alexanderdat98@gmail.com>
2025-12-28 18:15:14 +0700

    fix(router): enable mergeParams to access parent route params

--- src/routes/list.js
+++ src/routes/list.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 const { requireBoardAccess, requireBoardAdmin } = require("../middlewares/boardMiddleware");
 const ListController = require("../controllers/ListController");
